cc00a5eaf528f64b5c92a4bf0ff7ab6b7e11afda
Ayush Anbhore <33788899+Ayushanbhore@users.noreply.github.com>
2024-04-21 21:16:05 +0530

    Initial Commit

---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by Ayush Anbhore on 21/04/24.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
